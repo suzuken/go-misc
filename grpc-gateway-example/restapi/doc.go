238b5f09b759043662f99e835e52f1ed92aa1655
Kenta SUZUKI <suzuken326@gmail.com>
2018-03-01 15:53:29 +0900

    generated by swagger (server and client)

---
+++ grpc-gateway-example/restapi/doc.go
@@ -0,0 +1,20 @@
+/*Package restapi my_service.proto
+
+    Schemes:
+      http
+      https
+    Host: localhost
+    BasePath: /
+    Version: version not set
+
+    Consumes:
+    - application/json
+
+
+    Produces:
+    - application/json
+
+
+swagger:meta
+*/
+package restapi
